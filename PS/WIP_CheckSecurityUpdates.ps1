c7cc97f317fa250227722cc23c591c029c262bd2
lachlanalston <41813866+lachlanalston@users.noreply.github.com>
2025-06-18 11:11:39 +1000

    Update WIP_CheckSecurityUpdates.ps1
    Updated script to output if no updates are available

--- PS/WIP_CheckSecurityUpdates.ps1
+++ PS/WIP_CheckSecurityUpdates.ps1
@@ -1,1 +1,9 @@
+$updateSession = New-Object -ComObject Microsoft.Update.Session
+$searcher = $updateSession.CreateUpdateSearcher()
+$results = $searcher.Search("IsHidden=0 and IsInstalled=0")
 
+if ($results.Updates.Count -eq 0) {
+    Write-Output "No updates available."
+} else {
+    $results.Updates | Select-Object Title, Description, SupportUrl, IsMandatory
+}
