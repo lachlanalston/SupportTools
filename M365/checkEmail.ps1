56cde29374a34db30ba5320f7975983b77cf9ff0
Lachlan Alston <lachlan@alston.id.au>
2026-04-19 09:44:35 +1000

    Cleaned up M365 scripts

--- M365/checkEmail.ps1
+++
@@ -1,3 +0,0 @@
-Get-Recipient -ResultSize Unlimited | Where-Object { $_.EmailAddresses -match "admin@domain.com" } | Select Name,RecipientType,RecipientTypeDetails,PrimarySmtpAddress
-
-Get-DistributionGroupMember -Identity admin@domain.com | Select Name, PrimarySmtpAddress
